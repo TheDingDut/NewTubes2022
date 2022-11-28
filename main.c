b93664ef42e97970457383a8844e0edcfdf45ade
Fikri <fikreynursyabani@gmail.com>
2022-11-28 11:23:48 +0700

    update switch case

--- main.c
+++ main.c
@@ -6,40 +6,57 @@
 int main()
 {
     char pemain1[50], pemain2[50];
+    int opsi;
     cover();
     menuUtama();
     printf("\nPilih Menu : ");
-    getchar();
-    system("cls");
-    InputNamaPemain(&pemain1, &pemain2);
-    system("cls");
-    while (AkhirPermainan())
-    {
-        cetakPapan3();
-        giliran(pemain1, pemain2);
-        AkhirPermainan();
-    }
+    scanf("%d", &opsi);
 
-    if (XO == 'X' && !draw)
+    switch (opsi)
     {
+    case 1:
         system("cls");
-        printf("%s memenangkan permainan!", pemain1);
-        printf("\nHasil Akhir :\n");
-        cetakPapan3();
-    }
-    else if (XO == 'O' && !draw)
-    {
+        InputNamaPemain(&pemain1, &pemain2);
         system("cls");
-        printf("%s memenangkan permainan!", pemain2);
-        printf("\nHasil Akhir :\n");
-        cetakPapan3();
-    }
-    else
-    {
-        system("cls");
-        printf("Permainan Seri!");
-        printf("\nHasil Akhir :\n");
-        cetakPapan3();
+        while (AkhirPermainan())
+        {
+            cetakPapan3();
+            giliran(pemain1, pemain2);
+            AkhirPermainan();
+        }
+
+        if (XO == 'X' && !draw)
+        {
+            system("cls");
+            printf("%s memenangkan permainan!", pemain1);
+            printf("\nHasil Akhir :\n");
+            cetakPapan3();
+        }
+        else if (XO == 'O' && !draw)
+        {
+            system("cls");
+            printf("%s memenangkan permainan!", pemain2);
+            printf("\nHasil Akhir :\n");
+            cetakPapan3();
+        }
+        else
+        {
+            system("cls");
+            printf("Permainan Seri!");
+            printf("\nHasil Akhir :\n");
+            cetakPapan3();
+        }
+        break;
+    case 2:
+        /*code*/
+        break;
+    case 3:
+        printf("Terimakasih :)");
+        getchar();
+        break;
+    default:
+        printf("Input Salah!");
+        break;
     }
 
     return 0;
