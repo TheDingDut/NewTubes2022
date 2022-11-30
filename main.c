4ac05b9f2c16e262cb9c71ae7398a73490681991
Fikri <fikreynursyabani@gmail.com>
2022-11-30 20:05:19 +0700

    tambah loop + jumlah menang

--- main.c
+++ main.c
@@ -7,60 +7,76 @@ int main()
 {
     char pemain1[50], pemain2[50];
     int opsi;
-    cover();
-    system("cls");
-    menuUtama();
-    scanf("%d", &opsi);
-
-    switch (opsi)
+    char loopGame = 'N';
+    
+    do
     {
-    case 1:
+        /* code */
+        cover();
         system("cls");
-        InputNamaPemain(&pemain1, &pemain2);
-        system("cls");
-        while (AkhirPermainan())
+        menuUtama();
+        scanf("%d", &opsi);
+
+        switch (opsi)
         {
-            printf("Waktu anda hanya 10 detik, lebih dari itu maka giliran bermain akan terganti\n");
-            cetakPapan3();
-            giliran(pemain1, pemain2);
-            AkhirPermainan();
+        case 1:
+            system("cls");
+            InputNamaPemain(&pemain1, &pemain2);
+            system("cls");
+            while (AkhirPermainan())
+            {
+                printf("Waktu anda hanya 10 detik, lebih dari itu maka giliran bermain akan terganti\n");
+                cetakPapan3();
+                giliran(pemain1, pemain2);
+                AkhirPermainan();
+            }
+
+            if (XO == 'X' && !draw)
+            {
+                system("cls");
+                p1menang++;
+                printf("%s memenangkan permainan!", pemain1);
+                printf("\nHasil Akhir :\n");
+                cetakPapan3();
+            }
+            else if (XO == 'O' && !draw)
+            {
+                system("cls");
+                p2menang++;
+                printf("%s memenangkan permainan!", pemain2);
+                printf("\nHasil Akhir :\n");
+                cetakPapan3();
+            }
+            else
+            {
+                system("cls");
+                printf("Permainan Seri!");
+                printf("\nHasil Akhir :\n");
+                cetakPapan3();
+            }
+            break;
+        case 2:
+            /*code*/
+            break;
+        case 3:
+            printf("Terimakasih :)");
+            getchar();
+            break;
+        default:
+            printf("Input Salah!");
+            break;
         }
 
-        if (XO == 'X' && !draw)
-        {
-            system("cls");
-            p1menang++;
-            printf("%s memenangkan permainan!", pemain1);
-            printf("\nHasil Akhir :\n");
-            cetakPapan3();
-        }
-        else if (XO == 'O' && !draw)
-        {
-            system("cls");
-            p2menang++;
-            printf("%s memenangkan permainan!", pemain2);
-            printf("\nHasil Akhir :\n");
-            cetakPapan3();
-        }
-        else
-        {
-            system("cls");
-            printf("Permainan Seri!");
-            printf("\nHasil Akhir :\n");
-            cetakPapan3();
-        }
-        break;
-    case 2:
-        /*code*/
-        break;
-    case 3:
-        printf("Terimakasih :)");
-        getchar();
-        break;
-    default:
-        printf("Input Salah!");
-        break;
-    }
+        printf("\nUlangi permainan? ");
+        scanf("%s", &loopGame);
+    } while (loopGame == 'Y');
 
+    system("cls");
+    printf("%s berhasil memenangkan %d permainan!\n", pemain1, p1menang);
+    printf("%s berhasil memenangkan %d permainan!\n", pemain2, p2menang);
+
+    
+    
+    
     return 0;
 }
